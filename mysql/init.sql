9d04853d2ce0c5af71fd81257c982998c829d2b8
rosana <rosanariblim@gmail.com>
2023-09-24 23:00:02 -0300

    adicao de mysql

---
+++ mysql/init.sql
@@ -0,0 +1,3 @@
+create database desafio2db;
+use desafio2db;
+create table people(id int not null auto_increment, name varchar(255), primary key(id));
--- node/index.js
+++ node/index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'desafio2db'
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Rosana')`
+connection.query(sql)
 
 app.get('/',(req,res) => {
-    res.send('<h1>Full Cycle TESTE</h1>')
+    res.send('<h1>Full Cycle Rocks!</h1>')
+    connection.query("SELECT name FROM people", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+    });
 })
+
+connection.end()
 
 app.listen(port, () => {
     console.log('Rodando na porta ' +port)
